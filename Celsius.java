e496e366eb106a4db577e960c7f1227f3092756e
anupamamadane <128138474+anupamamadane@users.noreply.github.com>
2023-03-17 03:25:25 -0700

    Add files via upload

---
+++ Celsius.java
@@ -0,0 +1,15 @@
+class Celsius{
+	public static void main(String args[]){
+		
+		float ce =34.44f;
+		double far = ce * 1.80 + 32;
+		
+		System.out.println("Fahrenheit = " + far);
+		
+		double cel = (far - 32)* 0.5556;
+		
+		
+		System.out.println("celsius = " + cel);
+	
+	}
+} 
